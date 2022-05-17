a2208aae135e623f162181ca6ee904a8835007a8
dinnerhe <dinnerhe@hotmail.com>
2022-05-17 02:29:18 -0700

    Updated CSHomework1

---
+++ C-Sharp-HW1/SharpHomework1/UnderstandingTypes/ConvertCentury.cs
@@ -0,0 +1,24 @@
+﻿namespace UnderstandingTypes;
+
+public static class ConvertCentury
+{
+    public static void Convert(uint century)
+    {
+        uint year = century * 100;
+        
+        uint day = year * 365;
+        day += (year- year%4) / 4;
+        
+        //uint day = century * 36524;
+        ulong hour = day * (ulong)24;
+        ulong minute = hour * 60;
+        ulong second = minute * 60;
+        ulong millisecond = second * 1000;
+        ulong microsecond = millisecond * 1000;
+        ulong nanosecond = microsecond * 1000;
+        string result = String.Format("{0} centuries = {1} years = {2} days = {3} hours = {4} minutes" +
+                                      "= {5} seconds = {6} milliseconds = {7} microseconds = {8} nanoseconds",
+            century,year, day, hour,minute, second, millisecond, microsecond, nanosecond);
+        Console.WriteLine(result);
+    }
+}
---
+++ C-Sharp-HW1/SharpHomework1/UnderstandingTypes/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using UnderstandingTypes;
+
+string header = String.Format("{0,-10} {1, -10} {2, -30} {3, -30}\n", "Type", "Memory", "Minumum", "Maximum");
+Console.Write(header);
+Console.Write("{0,-10} {1, -10} {2, -30} {3, -30}\n", "sbyte", "1", "-128", "127");
+Console.Write("{0,-10} {1, -10} {2, -30} {3, -30}\n", "byte", "1", "0", "305");
+Console.Write("{0,-10} {1, -10} {2, -30} {3, -30}\n", "short", "2", "-32768", "32767");
+Console.Write("{0,-10} {1, -10} {2, -30} {3, -30}\n", "ushort", "2", "0", "65535");
+Console.Write("{0,-10} {1, -10} {2, -30} {3, -30}\n", "int", "4", "-2,147,483,648", "2,147,483,647");
+Console.Write("{0,-10} {1, -10} {2, -30} {3, -30}\n", "uint", "4", "0", "4,294,967,295");
+Console.Write("{0,-10} {1, -10} {2, -30} {3, -30}\n", "long", "8", "-9,223,372,036,854,775,808", "9,223,372,036,854,775,807");
+Console.Write("{0,-10} {1, -10} {2, -30} {3, -30}\n", "ulong", "8", "0", "18,446,744,073,709,551,615");
+Console.Write("{0,-10} {1, -10} {2, -30} {3, -30}\n", "float", "4", "±1.0e-45", "±3.4e38");
+Console.Write("{0,-10} {1, -10} {2, -30} {3, -30}\n", "double", "8", "±5e-324", "±1.7e308");
+Console.Write("{0,-10} {1, -10} {2, -30} {3, -30}\n", "decimal", "16", "±1.0 ×10e-28", "±7.9e28");
+ConvertCentury.Convert(5);
