a8d854fabe878dbefa8b16568fe01b56e3a2d8e9
dinnerhe <dinnerhe@hotmail.com>
2022-05-18 20:41:31 -0700

    Revert "Updated CSHomework1"
    This reverts commit 58c7022908ed1d34932d6b598ae18f67ae2a9508.

--- C-Sharp-HW1/SharpHomework1/Loops-Operators-Questions/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Loops_Operators_Questions;
-
-//sGuessNumber.Guess();
-//Console.WriteLine(CalculateAge.calc());
-//Greetings.Greets(new DateTime(2000, 12, 29, 14, 22, 22));
-//Greetings.Greets(DateTime.Now);
-Count24.Count();
